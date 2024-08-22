af524896045f49b34fc08e88c9c0a37a2f93e07a
yoounyoungheon <107925716+yoounyoungheon@users.noreply.github.com>
2024-08-22 15:36:51 +0900

    context-environment

--- hanaro-js/constext-study.js
+++ hanaro-js/constext-study.js
@@ -2,12 +2,13 @@
 hoisting
 var gg; var bb; let xx;
 */
+
 var gg =1; let bb=2;
 
 function f1(x,y){
     var gg=11; let bb=22;
     console.log('f1>', gg, bb, zz ,f2, f2.length);
-    f2('first');
+    f2('first'); // t,u,v
     {
         const xx=99;
         f2('nest-first'); // nested f2()실행
@@ -18,7 +19,7 @@ function f1(x,y){
     function f2(t,u){console.log(t,'inner', xx, zz)}
     function f2(t,u,v){console.log(t,'inner2', xx, zz)}
     var zz=800;
-    f2('second'); // block scope안 f2() 실행 -> nested?outerEnvReference ?
+    f2('second'); // block scope안 f2() 실행
 }
 
 function f2(g){ console.log(g, 'global f2', gg, bb, xx, kk) };
