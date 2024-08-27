3d6b509a2d074d7ba830440d570127b4b55be942
yoounyoungheon <107925716+yoounyoungheon@users.noreply.github.com>
2024-08-27 17:56:54 +0900

    ,

--- hanaro-js/object.js
+++ hanaro-js/object.js
@@ -11,7 +11,6 @@
 
 // const obj = makeObjectFromArray(arrays);
 
-
 // const kim = {id:3, name:'Kim', addr:'Seoul'}
 // function copyObject(obj){
 //     const newObj = {};
@@ -32,3 +31,56 @@
 //     }
 // };
 
+// function func() {
+//     const x = 10;
+//     return function() {
+//       console.log("x값은 :", x)
+//     }
+//   };
+// const myFunc = func();
+// myFunc();
+
+// class Func{
+//     #x =10;
+//     constructor(){};
+//     fun () {
+//         console.log("x값은 :", this.#x);
+//     }
+// };
+// const myFunc2 = new Func();
+// myFunc2.fun();
+
+// const arr = [1,2,3,4,5];
+// console.log(Array.prototype)
+// console.log(arr.__proto__) // 모든 배열은 프로토타입을 상속받음
+
+// const arr2 = Array.from(arr);// 깊은 복사
+// console.log(arr2 === arr); // falss
+
+// const arr3 = [...arr]; // 깊은 복사
+// console.log(arr3 === arr); // falss
+
+// const arr4 = arr; // 얕은 복사
+// console.log(arr4 === arr); // true 
+
+// const queue = [];
+// queue.unshift(1);
+// queue.unshift(2,3);
+// queue.unshift(4)
+// console.log(queue);
+// queue.shift();
+// console.log(queue);
+
+const arr = [1,2,3,4,5];
+console.log(arr.slice(1,3))
+console.log(arr.slice(2,))
+arr.splice(1,3);
+arr.splice(1,0,2,3,4);
+console.log(arr)
+arr.splice(2,)
+console.log(arr);
+arr.push(3,4,5)
+// arr.splice(2, 0, 'X', 'Y', 'Z');
+console.log(arr)
+const arr6 = [...arr.slice(0,2),'X','Y','Z',...arr.slice(2,)]
+console.log(arr6)
